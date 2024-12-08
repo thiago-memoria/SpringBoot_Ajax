65ad45349bc59074f5d60ba22395b18ac2813485
Thiago Barroso <memoriathiago@gmail.com>
2024-12-08 00:11:46 -0300

    Criação do projeto

---
+++ src/main/java/com/thiago/barroso/vendasapi/PromocoesApiApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.barroso.vendasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocoesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromocoesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/barroso/vendasapi/PromocoesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.barroso.vendasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromocoesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

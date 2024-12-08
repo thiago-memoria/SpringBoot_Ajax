2f6cdfeb70b5760afd83bfd95d7ac1aeb915fa62
Thiago Barroso <memoriathiago@gmail.com>
2024-12-08 10:19:17 -0300

    Modificação no domain e no ServiceMetaTag

--- src/main/java/com/thiago/barroso/vendasapi/PromocoesApiApplication.java
+++ src/main/java/com/thiago/barroso/vendasapi/PromocoesApiApplication.java
@@ -1,13 +1,29 @@
 package com.thiago.barroso.vendasapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.thiago.barroso.vendasapi.domain.SocialMetaTag;
+import com.thiago.barroso.vendasapi.service.SocialMetaTagService;
+
 @SpringBootApplication
-public class PromocoesApiApplication {
+public class PromocoesApiApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PromocoesApiApplication.class, args);
+	}
+	
+	@Autowired
+	SocialMetaTagService service;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		SocialMetaTag tag = service.getSocialMetaTagByUrl("https://www.mobly.com.br/cadeira-gamer-olympians-poseidon-tecido-preta-881514.html?origin=jetmobly&label=");
+		
+		System.out.println(tag.toString());
 	}
 
 }
---
+++ src/main/java/com/thiago/barroso/vendasapi/service/SocialMetaTagService.java
@@ -0,0 +1,80 @@
+package com.thiago.barroso.vendasapi.service;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.thiago.barroso.vendasapi.domain.SocialMetaTag;
+
+@Service
+public class SocialMetaTagService {
+	
+	private static Logger log = LoggerFactory.getLogger(SocialMetaTagService.class);
+	
+	public SocialMetaTag getSocialMetaTagByUrl(String url) {
+		SocialMetaTag twitter = getTwitterCardByUrl(url);
+		if(!isEmpty(twitter)) {
+			return twitter;
+		}
+		
+		SocialMetaTag openGraph = getOpenGraphByUrl(url);
+		if(!isEmpty(openGraph)) {
+			return openGraph;
+		}
+		
+		return null;
+	}
+	
+	private SocialMetaTag getOpenGraphByUrl(String url) {
+		
+		SocialMetaTag tag = new SocialMetaTag();
+		
+		try {
+			Document doc = Jsoup.connect(url).get();
+			
+			tag.setTitle(doc.head().select("meta[property=og:title]").attr("content"));
+			tag.setSite(doc.head().select("meta[property=og:site_name]").attr("content"));
+			tag.setImage(doc.head().select("meta[property=og:image]").attr("content"));
+			tag.setUrl(doc.head().select("meta[property=og:url]").attr("content"));
+			
+		}catch(IOException e) {
+			log.error(e.getMessage(), e.getCause());
+		}
+		
+		
+		return tag;
+	}
+	
+	private SocialMetaTag getTwitterCardByUrl(String url) {
+			
+			SocialMetaTag tag = new SocialMetaTag();
+			
+			try {
+				Document doc = Jsoup.connect(url).get();
+				
+				tag.setTitle(doc.head().select("meta[name=twitter:title]").attr("content"));
+				tag.setSite(doc.head().select("meta[name=twitter:site]").attr("content"));
+				tag.setImage(doc.head().select("meta[name=twitter:image]").attr("content"));
+				tag.setUrl(doc.head().select("meta[name=twitter:url]").attr("content"));
+				
+			}catch(IOException e) {
+				log.error(e.getMessage(), e.getCause());
+			}
+			
+			
+			return tag;
+		}
+	
+	public boolean isEmpty(SocialMetaTag tag) {
+		if(tag.getImage().isEmpty()) return true;
+		if(tag.getSite().isEmpty()) return true;
+		if(tag.getTitle().isEmpty()) return true;
+		if(tag.getUrl().isEmpty()) return true;
+		return false;
+	}
+	
+}

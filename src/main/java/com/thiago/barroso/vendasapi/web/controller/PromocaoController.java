eae5430dfc659bd8d815aa64f8d9c41c43d5d45b
Thiago Barroso <memoriathiago@gmail.com>
2024-12-10 22:09:50 -0300

    Adição do end point de salvar promoção

--- src/main/java/com/thiago/barroso/vendasapi/web/controller/PromocaoController.java
+++ src/main/java/com/thiago/barroso/vendasapi/web/controller/PromocaoController.java
@@ -1,22 +1,43 @@
 package com.thiago.barroso.vendasapi.web.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.thiago.barroso.vendasapi.domain.Categoria;
+import com.thiago.barroso.vendasapi.domain.Promocao;
 import com.thiago.barroso.vendasapi.repository.CategoriaRepository;
+import com.thiago.barroso.vendasapi.repository.PromocaoRepository;
 
 @Controller
 @RequestMapping("/promocao")
 public class PromocaoController {
 	
+	private static Logger log = LoggerFactory.getLogger(PromocaoController.class);
+	
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	PromocaoRepository promocaoRepository;
+	
+	@PostMapping("/save")
+	public ResponseEntity<Promocao> salvarPromocao(Promocao promocao){
+		
+		log.info("Promocao {}", promocao.toString());
+		promocao.setDtCadastro(LocalDateTime.now());
+		promocaoRepository.save(promocao);
+		return ResponseEntity.ok().build();
+	}
 	
 	@ModelAttribute("categorias")
 	public List<Categoria> getCategorias(){
